fd71e63da8334f2ea82913dcde62df0541fa42c7
Guillaume Mazoyer <respawneral@gmail.com>
2009-11-30 18:15:34 +0100

    Add 2 more options.   * Calculate the MD5 sum of the file if the algorihtm can.   * Remove the parts file if the merge was successful.

--- src/org/gnome/split/gtk/dialog/PreferencesDialog.java
+++ src/org/gnome/split/gtk/dialog/PreferencesDialog.java
@@ -30,7 +30,6 @@ import org.gnome.gtk.Notebook;
 import org.gnome.gtk.PositionType;
 import org.gnome.gtk.ResponseType;
 import org.gnome.gtk.Stock;
-import org.gnome.gtk.Table;
 import org.gnome.gtk.VButtonBox;
 import org.gnome.gtk.Widget;
 import org.gnome.gtk.Window;
@@ -45,14 +44,40 @@ import static org.freedesktop.bindings.Internationalization._;
 
 public class PreferencesDialog extends Dialog implements DeleteEvent, Response
 {
+    /**
+     * Configuration of the application.
+     */
     private final Configuration config;
 
+    /**
+     * Current instance of the application.
+     */
     private GnomeSplit app;
 
+    /**
+     * Button to know if we should calculate the MD5 sum.
+     */
+    private CheckButton md5sum;
+
+    /**
+     * Button to know if we should remove the parts files.
+     */
+    private CheckButton remove;
+
+    /**
+     * Button to know if we should inhibit the hibernation during an action.
+     */
     private CheckButton hibernation;
 
+    /**
+     * Button to know if we should display an icon in the notification area.
+     */
     private CheckButton trayIcon;
 
+    /**
+     * Button to know if we should display notifications when an action
+     * finished.
+     */
     private CheckButton notification;
 
     public PreferencesDialog(final GnomeSplit app) {
@@ -79,24 +104,56 @@ public class PreferencesDialog extends Dialog implements DeleteEvent, Response
         this.connect((Dialog.Response) this);
     }
 
+    /**
+     * Create the first page of the {@link Notebook notebook} (general
+     * config).
+     */
     private Alignment createGeneralPage() {
         final Alignment page = new Alignment(0.0f, 0.0f, 0.0f, 0.0f);
         page.setPadding(5, 5, 20, 5);
 
-        final Table table = new Table(1, 2, false);
-        table.setRowSpacing(3);
-        table.setColumnSpacing(3);
-        page.add(table);
+        md5sum = new CheckButton(_("_Calculate the MD5 if possible."));
+        md5sum.setActive(config.SAVE_FILE_HASH);
+        md5sum.connect(new Button.Clicked() {
+            @Override
+            public void onClicked(Button source) {
+                // Save preferences
+                config.SAVE_FILE_HASH = md5sum.getActive();
+                config.savePreferences();
+            }
+        });
+
+        remove = new CheckButton(_("_Remove parts if the merge was successful."));
+        remove.setActive(config.DELETE_PARTS);
+        remove.connect(new Button.Clicked() {
+            @Override
+            public void onClicked(Button source) {
+                // Save preferences
+                config.DELETE_PARTS = remove.getActive();
+                config.savePreferences();
+            }
+        });
+
+        // Pack buttons in the box
+        final VButtonBox vbox = new VButtonBox();
+        page.add(vbox);
+
+        // Add every options
+        vbox.add(md5sum);
+        vbox.add(remove);
 
         return page;
     }
 
+    /**
+     * Create the last page of the {@link Notebook notebook} (desktop config).
+     */
     private Alignment createDesktopPage() {
         final Alignment page = new Alignment(0.0f, 0.0f, 0.0f, 0.0f);
         page.setPadding(5, 5, 20, 5);
 
         // Restore hibernation status
-        hibernation = new CheckButton(_("Inhibit desktop _hibernation when action is performed"));
+        hibernation = new CheckButton(_("Inhibit desktop _hibernation when action is performed."));
         hibernation.setActive(config.NO_HIBERNATION);
         hibernation.connect(new Button.Clicked() {
             @Override
@@ -108,7 +165,7 @@ public class PreferencesDialog extends Dialog implements DeleteEvent, Response
         });
 
         // Restore tray icon status
-        trayIcon = new CheckButton(_("Show _icon in the desktop notification area"));
+        trayIcon = new CheckButton(_("Show _icon in the desktop notification area."));
         trayIcon.setActive(config.SHOW_TRAY_ICON);
         trayIcon.connect(new Button.Clicked() {
             @Override
@@ -123,7 +180,7 @@ public class PreferencesDialog extends Dialog implements DeleteEvent, Response
         });
 
         // Restore notifications status
-        notification = new CheckButton(_("Show desktop _notification"));
+        notification = new CheckButton(_("Show desktop _notification."));
         notification.setActive(config.USE_NOTIFICATION);
         notification.connect(new Button.Clicked() {
             @Override
