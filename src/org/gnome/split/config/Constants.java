9e95f0baf4693fe746aabfb82d7433c92449fdc5
Guillaume Mazoyer <respawneral@gmail.com>
2010-02-12 10:43:52 +0100

    Bump version to 0.6 for next development cycle.

--- src/org/gnome/split/config/Constants.java
+++ src/org/gnome/split/config/Constants.java
@@ -39,7 +39,7 @@ public final class Constants
     /**
      * Version of the program.
      */
-    public static String PROGRAM_VERSION = "0.5";
+    public static String PROGRAM_VERSION = "0.6";
 
     /**
      * Website of the program.
